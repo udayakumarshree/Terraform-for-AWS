04983dadf6084f9d0491c98913751f6a52ae4c5d
udayakumarshree <71256944+udayakumarshree@users.noreply.github.com>
2024-09-25 00:47:33 +0530

    Update backend.tf

--- Non-Modularized/VPC/backend.tf
+++ Non-Modularized/VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
+    bucket         = "udaysec-report-ssm"
+    region         = "ap-south-1"
     key            = "Non-Modularized/VPC/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt = true
